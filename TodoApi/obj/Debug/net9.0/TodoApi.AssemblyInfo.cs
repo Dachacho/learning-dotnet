76d949edc24ba40c4c368a870951c399ded96026
Dachacho <arevadzedachi123@gmail.com>
2025-08-17 16:30:12 +0400

    added a DTO to cover the secret in the Todo model

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964bd8d6c2932b0d77430af3b3bfadc7ea139884")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fffee1fd9e952d916b37aa410bbbc9702d648931")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
